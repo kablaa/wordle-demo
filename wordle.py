712c9f6f871c503027222b4a60ebf7879c9a9da5
kablaa <kablaa@users.noreply.github.com>
2023-02-22 17:55:06 +0000

    adding more formatting

--- wordle.py
+++ wordle.py
@@ -11,12 +11,13 @@ URL = 'https://www.nytimes.com/svc/wordle/v2/{}.json'
 
 for i in range(0,30):
     date = datetime.today() + timedelta(i)
-    req_url = URL.format(date.strftime('%Y-%m-%d'))
+    formated_date = date.strftime('%Y-%m-%d')
+    req_url = URL.format(formated_date)
     print('request: ' + req_url)
     try:
         with request.urlopen(req_url) as response:
             r = json.loads(response.read())
             print('response: ' + json.dumps(r))
+            print('solution for ' + formated_date + ': ' + r['solution'] + '\n')
     except http_error.HTTPError:
         break
-    
