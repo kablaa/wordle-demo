06d88e010cbc2962d0caa6ec2a4fb9cfbee2fca7
kablaa <kablaa@users.noreply.github.com>
2023-02-22 17:49:20 +0000

    making nicer

--- wordle.py
+++ wordle.py
@@ -1,13 +1,22 @@
+'''
+Formats the json name with the current date and iterates over dates
+until a 404 error is returned.
+'''
 from datetime import datetime, timedelta
-import urllib.request
+from urllib import request, error as http_error
+import json
 
-url = 'https://www.nytimes.com/svc/wordle/v2/{}.json'
+URL = 'https://www.nytimes.com/svc/wordle/v2/{}.json'
+
+
 for i in range(0,30):
     date = datetime.today() + timedelta(i)
-    req_url = url.format(date.strftime('%Y-%m-%d'))
+    req_url = URL.format(date.strftime('%Y-%m-%d'))
+    print('request: ' + req_url)
     try:
-        with urllib.request.urlopen(req_url) as response:
-            r = response.read()
-            print(r)
-    except:
+        with request.urlopen(req_url) as response:
+            r = json.loads(response.read())
+            print('response: ' + json.dumps(r))
+    except http_error.HTTPError:
         break
+    
